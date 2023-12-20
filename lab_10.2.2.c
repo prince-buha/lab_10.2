89e2c75cf133bec880e1b31d313fdd7a75ab18ee
prince-buha <150029430+prince-buha@users.noreply.github.com>
2023-12-20 17:59:48 +0530

    Update lab_10.2.2.c

--- lab_10.2.2.c
+++ lab_10.2.2.c
@@ -1,12 +1,18 @@
+/*
+Q.2 Write a Program to find the length of the String by passing a string as an argument using UDF.
+For example,
+*/
 #include<stdio.h>
-
-int string(char text){
+char CharLen(char text){
 	return text;
-	
 }
 void main(){
 	char alpha[100];
+	int length=0,i;
 	printf("Enter any String: ");
 	gets(alpha);
-	printf("The Length of String is %d",strlen(alpha));
+	for(i=0;i<alpha[i];i++){
+		length++;
+	}
+	printf("The Length of Array is %d",length);
 }
